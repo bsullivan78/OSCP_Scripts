529baa24dc3551c185887545938e260f30e9be2f
Brandon <brandonsullivan78@gmail.com>
2021-01-28 19:26:53 -0500

    Step 1 Script
    Input an IP, creates a folder for info (screenshots, notes, process, scripts, etc) creates a "process" file (a file to put steps taken). Begin a NMap syn scan, then from the open ports, begin a Version Scan on the open ports. Then open the resulting version file.

---
+++ StartScan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+#checks for proper format
+if [ -z $1 ]
+then	
+	echo "Requires IP Ex: \"proofofconcept.sh 1.1.1.1\""
+	exit
+fi
+
+#find the ip and create a folder with nessasary files
+h=$(echo $1 | cut -d'.' -f4)
+loc="/home/kali/Documents/"${h}		#change to which directory to use.
+mkdir $loc
+cd $loc
+touch scratch.txt
+touch process.txt
+echo "Port Scan" >> ./process.txt
+
+#nmap syn scan
+sudo nmap -sS $1 -p1-65535 -oN "${h}sN.txt"
+
+declare -a ports
+while IFS= read -r line; do
+	if [[ $line == *"open"* ]]; then
+		p=$(echo $line | cut -d'/' -f1)
+		ports+="${p},"
+	fi
+done < ./${h}sN.txt
+str=${ports[*]}
+
+#nmap Version scan
+sudo nmap -sV $1 -p${str%?} -oN "${h}sV.txt"
+
+mousepad ./${h}sV.txt
+exit
